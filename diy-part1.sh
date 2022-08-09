12bf3bedb0f8a9efda4f984529f3bed545dbbb55
lvzhongpei <42686513+lvzhongpei@users.noreply.github.com>
2022-08-09 19:02:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,8 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*passwall\)/\1/' feeds.conf.default
-# sed -i 's/^#\(.*passwall2\)/\1/' feeds.conf.default
-# sed -i 's/^#\(.*aliyundrive\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-# echo 'src-svn aliyundrive https://github.com/messense/aliyundrive-webdav/trunk/openwrt' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
